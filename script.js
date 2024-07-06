6731c909405667ce9b2a2f84a3408f5cba4f5320
Julian-T-86 <iulianturlica86@gmail.com>
2024-07-06 21:28:31 +0100

    adjust to border shadow colors

--- script.js
+++ script.js
@@ -4,12 +4,12 @@
         let form = document.forms['EmailForm'];
     
         let hue = Math.floor(Math.random() * 361);
-        let saturationLight = Math.floor(Math.random() * 101);
-        let alpha = Math.random();
+        let saturation = Math.floor(Math.random() * 101);
+        let alpha = Math.random()/2 + 0.5;
     
-        about.style.boxShadow = `0px 0px 55px 5px hsla(${hue},${saturationLight}%,${Math.floor(saturationLight/2)}%,${alpha})`;
-        story.style.boxShadow = `0px 0px 35px 15px hsla(${hue},${saturationLight}%,${Math.floor(saturationLight/2)}%,${alpha})`;
-        form.style.boxShadow = `0px 0px 35px 15px hsla(${hue},${saturationLight}%,${Math.floor(saturationLight/2)}%,${alpha})`;
+        about.style.boxShadow = `0px 0px 55px 5px hsla(${hue},${saturation}%,50%,${alpha})`;
+        story.style.boxShadow = `0px 0px 35px 15px hsla(${hue},${saturation}%,50%,${alpha})`;
+        form.style.boxShadow = `0px 0px 35px 15px hsla(${hue},${saturation}%,50%,${alpha})`;
     }
     window.addEventListener('scroll', colorGenerator);
 
