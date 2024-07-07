70b66b58c16a0e440dfb7046ba293cbfbb4b2d5d
Julian-T-86 <iulianturlica86@gmail.com>
2024-07-07 15:11:46 +0100

    add projects iframes

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
         let about = document.getElementById('about');
         let story = document.getElementById('story');
         let form = document.forms['EmailForm'];
+        let project = document.getElementsByClassName('project');
     
         let hue = Math.floor(Math.random() * 361);
         let saturation = Math.floor(Math.random() * 101);
@@ -10,6 +11,13 @@
         about.style.boxShadow = `0px 0px 55px 5px hsla(${hue},${saturation}%,50%,${alpha})`;
         story.style.boxShadow = `0px 0px 35px 15px hsla(${hue},${saturation}%,50%,${alpha})`;
         form.style.boxShadow = `0px 0px 35px 15px hsla(${hue},${saturation}%,50%,${alpha})`;
+        for (let i=0; i < project.length; i++) {
+            project[i].style.boxShadow = `0px 0px 35px 15px hsla(${hue},${saturation}%,50%,${alpha})`;
+        }
+
+        setInterval(colorGenerator, 1500);
     }
-    window.addEventListener('scroll', colorGenerator);
+
+    colorGenerator();
+    //window.addEventListener('scroll', colorGenerator);
 
